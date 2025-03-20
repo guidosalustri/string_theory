de29e494c7c50eced4709c7c2212959ad3440322
SenPie <smbat@duck.com>
2025-03-20 23:06:14 +0100

    Log game complete

--- string_theory_1.0.0/objects/game_manager.gd
+++ string_theory_1.0.0/objects/game_manager.gd
@@ -21,6 +21,7 @@ func call_cutscene() -> void:
 	if lvl == lvls.size() - 1:# tendria que ser una flag corre la ultima cutscene y dsp entra a la final
 		# this could just go in the lvls array as "last lvl (13)"
 		# but is actually just a final cinematic or something.
+		data_collection.log_game_complete()
 		get_tree().change_scene_to_packed(final_scene)
 	else:
 		get_tree().change_scene_to_packed(cutscene)
