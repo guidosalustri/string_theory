224ed731c415efe472db780efdc877ad87c4dcb9
SenPie <smbat@duck.com>
2025-03-21 16:55:40 +0100

    Bring back old speedometer and energy bar

--- string_theory_1.0.0/ui/hud/hud.gd
+++ string_theory_1.0.0/ui/hud/hud.gd
@@ -2,9 +2,8 @@ extends Control
 
 @onready var timer_overcharged: Timer = $TimerOvercharged
 @onready var stars_progress: StarsProgress = $StarsProgress
-@onready var speed: Gauge = $SpeedAndEnergy
-@onready var energy: EnergyHUD = $Energy
-@onready var texture_progress_bar: TextureProgressBar = %TextureProgressBar
+@onready var fuel_bar_rainbow: FuelBarRainbow = $FuelBarRainbow
+@onready var speedometer_rainbow: SpeedometerRainbow = $SpeedometerRainbow
 
 @export var color_fuel_low := Color(1, 0.188, 0.188)
 @export var color_fuel_high := Color(0.141, 1, 0.282)
@@ -33,31 +32,22 @@ func _ready() -> void:
 	timer_overcharged.timeout.connect(_on_timer_overcharged_timeout)
 
 func _process(_delta: float) -> void:
-	var ship_speed : int = clamp(player.speed, 0, player.max_speed_hud)
-	speed.main_gauge_label_value = snapped(ship_speed,20)
-	speed.main_charge = ship_speed / player.max_speed
-
 	if player.fuel <= 0:
 		no_energy.emit()
-		#speed_and_energy.animation_player.play("empty_charge")
 	
 	if player.fuel >= player.max_fuel and timer_overcharged.is_stopped():
 		timer_overcharged.start()
-		##speed_and_energy.animation_player.play("full_charge")
+		fuel_bar_rainbow.animation_player.play("full_energy")
 #
 	if 0 < player.fuel and player.fuel < player.max_fuel:
 		if not timer_overcharged.is_stopped():
 			timer_overcharged.stop()
-			#speed_and_energy.animation_player.stop()
-		#if speed_and_energy.animation_player.current_animation == "empty_charge":
-			#speed_and_energy.animation_player.stop()
-
-	energy.left_thruster_charge = player.fuel_left / player.max_fuel
-	energy.main_thruster_charge = player.fuel / player.max_fuel
-	energy.right_thruster_charge = player.fuel_right / player.max_fuel
+			fuel_bar_rainbow.animation_player.stop()
 	
-	texture_progress_bar.value = player.fuel / player.max_fuel * 100
-	texture_progress_bar.tint_progress = color_fuel_low.lerp(color_fuel_high, player.fuel / player.max_fuel)
+	speedometer_rainbow.max_speed = player.max_speed_hud
+	speedometer_rainbow.speed = player.speed
+	fuel_bar_rainbow.max_fuel = player.max_fuel
+	fuel_bar_rainbow.fuel = player.fuel
 
 func texture_rect_tween(tex_rect: TextureRect) -> void:
 	var tween := create_tween().set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_OUT)
@@ -65,5 +55,6 @@ func texture_rect_tween(tex_rect: TextureRect) -> void:
 	tween.parallel().tween_property(tex_rect, "scale", Vector2(1.2,1.2), 0.1)
 
 func _on_timer_overcharged_timeout() -> void:
+	fuel_bar_rainbow.animation_player.stop()
 	overcharged.emit()
 	set_process(false)
