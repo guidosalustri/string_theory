3ab2c21ef1be0ad0c11699bd4216b60a3e91e243
guidosalustri <119360285+guidosalustri@users.noreply.github.com>
2025-03-24 12:31:58 -0300

    game working and all (aries_to virgo)

--- string_theory_1.0.0/objects/game_manager.gd
+++ string_theory_1.0.0/objects/game_manager.gd
@@ -9,7 +9,7 @@ extends Node
 
 var data_collection_impl := preload("res://objects/data_collection/data_collection_impl.gd")
 
-var lvl := 1
+var lvl := 0
 
 var deaths_counts := 0
 #var martin_on_ship := false
@@ -23,7 +23,7 @@ var _lvl_fresh_start := false
 
 func call_cutscene() -> void:
 	_lvl_fresh_start = true
-	if lvl == lvls.size() - 1:# tendria que ser una flag corre la ultima cutscene y dsp entra a la final
+	if lvl == lvls.size():# - 1:# tendria que ser una flag corre la ultima cutscene y dsp entra a la final
 		# this could just go in the lvls array as "last lvl (13)"
 		# but is actually just a final cinematic or something.
 		data_collection.log_game_complete()
