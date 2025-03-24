9d6f4603fbe3b3344b57d29f25223f807faa1108
guidosalustri <119360285+guidosalustri@users.noreply.github.com>
2025-03-24 11:05:12 -0300

    bh lights and speedometer fix

--- string_theory_1.0.0/ui/speedometer_rainbow/speedometer_rainbow.gd
+++ string_theory_1.0.0/ui/speedometer_rainbow/speedometer_rainbow.gd
@@ -9,5 +9,7 @@ var max_speed := 700.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	front.material.set_shader_parameter("value", (speed * 0.45)/ max_speed)
-	label.text = str(snapped(int(speed),25))
+	var speed_speedometer :float = min((speed * 0.45)/ max_speed,0.45)
+	var speed_label :float = min(speed, max_speed)
+	front.material.set_shader_parameter("value", speed_speedometer)
+	label.text = str(snapped(int(speed_label),25))
